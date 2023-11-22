41bba8f007f9cc013dc04e90e98a666a8688c039
kjw1025_mac_studio <126055193+KJWHUB@users.noreply.github.com>
2023-11-22 18:57:33 +0900

    layout padding 변경

--- src/components/layout/LeafBgLayout.tsx
+++ src/components/layout/LeafBgLayout.tsx
@@ -1,9 +1,14 @@
+import './leafBgLayout.scss'
+
 import Controller from '@/components/Controller/Controller'
 import Bg from '@/components/bg/ConceptBg'
 
 const LeafBgLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main style={{ position: 'relative' }}>
+    <main
+      className="leaf-bg-layout"
+      style={{ width: '100vw', height: '100vh' }}
+    >
       <Bg />
 
       <Controller>{children}</Controller>
