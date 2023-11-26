de97521d0badbd83792b01e56a156356da21e6a5
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-11-26 18:17:36 +0900

    반응형 프로젝트 상세 제외 1차

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,11 +1,7 @@
-import Link from 'next/link'
-import ProjectGlassCard from '@/components/Card/ProjectGlassCard'
+import ProjectList from '@/components/list/ProjectList'
 import WhiteTitleText from '@/components/text/WhiteTitleText'
 
-import data from '../../../db.json'
-
 export default function Page() {
-  const list = data.project
   return (
     <>
       <WhiteTitleText
@@ -15,32 +11,7 @@ export default function Page() {
         PROJECT
       </WhiteTitleText>
 
-      <ul
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        {list.map((item, i) => {
-          return (
-            <li
-              key={i}
-              className="animate__animated animate__fadeIn"
-              style={{
-                width: '48%',
-                margin: '0.5rem',
-                animationDelay: `0.${i}s`,
-              }}
-            >
-              <Link href={'/project/' + item.id}>
-                <ProjectGlassCard projectInfo={item} />
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+      <ProjectList />
     </>
   )
 }
