58e2a0fbeff38ee059130274a19e37dc4ce95237
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-08-10 15:01:22 +0900

    setup

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+
+function getDirectoriesToSort() {
+  const ignoredSortingDirectories = [
+    '.git',
+    '.next',
+    '.vscode',
+    'node_modules',
+  ];
+  return getDirectories(process.cwd()).filter(
+    (f) => !ignoredSortingDirectories.includes(f),
+  );
+}
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
+}
+
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  plugins: ['import'],
+  rules: {
+    'import/order': [
+      2,
+      {
+        groups: [
+          'external',
+          'builtin',
+          'internal',
+          'sibling',
+          'parent',
+          'index',
+        ],
+        pathGroups: [
+          ...getDirectoriesToSort().map((singleDir) => ({
+            pattern: `${singleDir}/**`,
+            group: 'internal',
+          })),
+          {
+            pattern: 'env',
+            group: 'internal',
+          },
+          {
+            pattern: 'theme',
+            group: 'internal',
+          },
+          {
+            pattern: 'public/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
